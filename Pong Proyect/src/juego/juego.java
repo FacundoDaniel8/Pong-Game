bf92f550585112b1980a1e2df12bac2de0733ce5
FacundoDaniel8 <86319792+FacundoDaniel8@users.noreply.github.com>
2023-10-10 10:32:49 -0300

    Add files via upload

---
+++ Pong Proyect/src/juego/juego.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juego;
+
+import clases.Panel;
+import javax.swing.JFrame;
+/**
+ *
+ * @author ALUMNOS
+ */
+public class juego {
+    public static void main(String[] args){
+        
+        Panel panel = new Panel();
+    }
+    
+}
