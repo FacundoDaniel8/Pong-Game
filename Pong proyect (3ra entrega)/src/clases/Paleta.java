6e9ace348f022cef8dc5a5af9298967515de5443
FacundoDaniel8 <86319792+FacundoDaniel8@users.noreply.github.com>
2023-10-22 15:47:00 -0300

    Add files via upload

---
+++ Pong proyect (3ra entrega)/src/clases/Paleta.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Paleta {
+    
+    public int x, y;
+    static final int ancho=96, alto=10;
+    
+    public Paleta(int x, int y){
+        
+        this.x=x;
+        this.y=y;
+    }
+    
+    public Rectangle2D getPaleta(){
+        
+        return new Rectangle2D.Double(x,y,ancho,alto);
+    }
+    
+    public void mover(Rectangle limites){
+        
+        if (!(Bola.finJuego)){
+            
+            if ((EventoTeclado.a || EventoTeclado.left) && x > limites.getMinX()){
+            
+                x -= 2;
+            }
+            if ((EventoTeclado.d || EventoTeclado.right) && x < limites.getMaxX()-ancho){
+            
+                x += 2;
+            }
+        }
+        
+    }
+}
